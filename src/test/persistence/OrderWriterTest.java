1c1b8cd58f5cc27897bf6bcf2c5be26202c474f1
Chhavi Nayyar <115122937+chhaviiiii@users.noreply.github.com>
2023-10-27 11:28:35 -0700

    PHASE 2: 11:30AM

--- src/test/persistence/OrderWriterTest.java
+++ src/test/persistence/OrderWriterTest.java
@@ -3,10 +3,14 @@ package persistence;
 import model.Order;
 import model.OrderStatus;
 import model.Product;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +21,7 @@ public class OrderWriterTest {
     @Test
     void testOrderWriter() {
         try {
-            OrderWriter orderWriter = new OrderWriter("test.json");
+            OrderWriter orderWriter = new OrderWriter("./data/test.json");
             assertNotNull(orderWriter);
         } catch (Exception e) {
             fail("Exception should not have been thrown.");
@@ -27,23 +31,42 @@ public class OrderWriterTest {
     @Test
     void testOpen() {
         try {
-            OrderWriter orderWriter = new OrderWriter("test.json");
+            OrderWriter orderWriter = new OrderWriter("./data/test.json"); // valid path
             orderWriter.open();
             assertNotNull(orderWriter);
         } catch (FileNotFoundException e) {
-            fail("File should be found and opened");
+            fail("File should be opened without exceptions");
         }
     }
 
     @Test
     void testWrite() {
         try {
-            OrderWriter orderWriter = new OrderWriter("test.json");
+            OrderWriter orderWriter = new OrderWriter("./data/test.json"); // valid path
             List<Order> orders = new ArrayList<>();
+            OrderStatus orderStatus = OrderStatus.COMPLETE;
+            String orderID = "o1";
+            String productDetails = "productDetails";
+            String customerDetails = "customerDetails";
+            List<Product> productsToSell = new ArrayList<>();
+            Order newOrder = new Order(orderID, productDetails, customerDetails, orderStatus, productsToSell);
+            orders.add(newOrder);
             orderWriter.write(orders);
 
-            File file = new File("test.json");
+            // Verify that JSON file exists
+            File file = new File("./data/test.json");
             assertTrue(file.exists());
+
+            // Read the file content
+            String jsonContent = new String(Files.readAllBytes(Paths.get("./data/test.json")));
+
+            // Verify that JSON file content is as expected
+            JSONObject expectedJsonObject = new JSONObject();
+            JSONArray jsonArray = new JSONArray();
+            jsonArray.put(newOrder.toJson());
+            expectedJsonObject.put("orders", jsonArray);
+            assertEquals(expectedJsonObject.toString(4), jsonContent.trim());
+
         } catch (Exception e) {
             fail("Writing should not result in an exception");
         }
@@ -58,7 +81,7 @@ public class OrderWriterTest {
     @Test
     void testClose() {
         try {
-            OrderWriter orderWriter = new OrderWriter("test.json");
+            OrderWriter orderWriter = new OrderWriter("./data/test.json");
             orderWriter.open();
             orderWriter.close();
 
@@ -69,6 +92,15 @@ public class OrderWriterTest {
             fail("Closing should not result in an exception");
         }
     }
+
+    @Test
+    void testOpenThrowsException() {
+        assertThrows(FileNotFoundException.class, () -> {
+            OrderWriter orderWriter = new OrderWriter("nonexistent/directory/test.json"); // invalid path
+            orderWriter.open();
+        }, "FileNotFoundException should be thrown for non-existent file or directory");
+    }
+
 
     @Test
     void testWrite_ThrowsException_WhenFileNotFound() {
@@ -92,7 +124,7 @@ public class OrderWriterTest {
     @Test
     void testJsonObjectCreationFromOrders() {
         try {
-            OrderWriter orderWriter = new OrderWriter("test.json");
+            OrderWriter orderWriter = new OrderWriter("./data/test.json");
             List<Order> orders = new ArrayList<>();
 
             // Creating dummy Order objects
@@ -118,7 +150,7 @@ public class OrderWriterTest {
             orderWriter.close();
 
             // Read the JSON file and check if it contains the added Orders
-            OrderReader orderReader = new OrderReader("test.json");
+            OrderReader orderReader = new OrderReader("./data/test.json");
             List<Order> readOrders = orderReader.read();
 
             assertEquals(2, readOrders.size());
@@ -128,7 +160,9 @@ public class OrderWriterTest {
         } catch (Exception e) {
             fail("Exception should not have been thrown.");
         }
+
     }
+
 }
 
 
